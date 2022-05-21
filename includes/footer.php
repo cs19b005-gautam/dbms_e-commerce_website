433ecfacd7b0cc8751d3a8ae50dc8fda3b74aa04
unknown <yaswanthamasa@gmail.com>
2022-05-21 21:51:53 +0530

    All changes done successfully

--- includes/footer.php
+++ includes/footer.php
@@ -84,36 +84,12 @@
 		</div>
 	</div>
 
-	<!-- <div class="banners">
-		<div class="container clearfix">
-
-			<div class="banner-award">
-				<span>Award winner</span><br> Fashion awards 2016
-			</div>
-
-			<div class="banner-social">
-				<a href="#" class="banner-social__link">
-				<i class="icon-facebook"></i>
-			</a>
-				<a href="#" class="banner-social__link">
-				<i class="icon-twitter"></i>
-			</a>
-				<a href="#" class="banner-social__link">
-				<i class="icon-instagram"></i>
-			</a>
-				<a href="#" class="banner-social__link">
-				<i class="icon-pinterest-circled"></i>
-			</a>
-			</div>
-
-		</div>
-	</div> -->
 
 	<div class="page-footer__subline">
 		<div class="container clearfix">
 
 			<div class="copyright">
-				&copy; 2018 Avenue Fashion&trade;
+				&copy; 2022 Shopee&trade;
 			</div>
 
 			<div class="developer">
